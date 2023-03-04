7601ab293a85ae67b71cb087d99c3dd610a79fe5
zine-fj <747810974@qq.com>
2023-03-04 11:44:23 +0800

    🚧 babelrc和postcss

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,26 @@ const config = {
         loader: "vue-loader",
       },
       {
+        test: /\.jsx$/,
+        loader: "babel-loader",
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
       {
         test: /\.styl$/,
-        use: ["style-loader", "css-loader", "stylus-loader"],
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          "stylus-loader",
+        ],
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
