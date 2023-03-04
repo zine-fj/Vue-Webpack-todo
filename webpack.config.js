92f76a1599d80743aa313883fa97d9e14556bc17
zine-fj <747810974@qq.com>
2023-03-04 11:13:44 +0800

    🚧 webpack-dev-server的配置和使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
-const path = require('path')
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin"); // 打包页面
+const webpack = require("webpack");
 
-module.exports = {
+const isDev = process.env.NODE_ENV === "development";
+
+const config = {
+  target: "web", // 编译目标是web平台
   // 入口 (拼接地址)
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: path.join(__dirname, "src/index.js"),
 
   // 出口
   output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
   },
 
   // 配置
@@ -16,35 +21,61 @@ module.exports = {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.styl$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'stylus-loader'
-        ]
+        use: ["style-loader", "css-loader", "stylus-loader"],
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
         use: [
           {
-            loader: 'url-loader', // 可以将图片转为 base64代码
-            options: { // 配置
+            loader: "url-loader", // 可以将图片转为 base64代码
+            options: {
+              // 配置
               limit: 1024, // 小于1024转为base64
-              name: '[name]-aaa.[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  }
+              name: "[name]-aaa.[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      // 必须要有，配置环境 开发环境有错误提醒，生产不需要
+      "process.env": {
+        NODE_ENV: isDev ? '"development"' : '"production"',
+      },
+    }),
+    new HTMLPlugin(),
+  ],
+};
+
+if (isDev) {
+  config.devtool = "#cheap-module-eval-source-map"; // 查找问题方便，不是编译后的代码
+  (config.devServer = {
+    port: "8000",
+    host: "0.0.0.0", // 可用两种方式查看
+    overlay: {
+      // 有错误就显示在网页上
+      errors: true,
+    },
+    hot: true,
+
+    // historyFallback: {}
+    // open: true
+  }),
+    config.plugins.push(
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    );
 }
+
+module.exports = config;
